dd3d006eec8de9dbdc7295dc29d1019c498a0e66
Touricks <f36meng@gmail.com>
2024-12-05 22:13:42 -0500

    update 2024/12/05

--- src/main/java/com/laioffer/twitch/external/TwitchApiClient.java
+++ src/main/java/com/laioffer/twitch/external/TwitchApiClient.java
@@ -26,8 +26,17 @@ public interface TwitchApiClient {
 
 
     @GetMapping("/videos")
-    VideoResponse getVideos(@RequestParam("game_id") String gameId, @RequestParam("first") int first);
+    VideoResponse getVideos(
+            @RequestParam(value = "game_id", required = false) String gameId,
+            @RequestParam(value = "first", required = false, defaultValue = "0") int first,
+            @RequestParam(value = "id", required = false) List<String> VideoId);
 
+    /*
+    @GetMapping("/videos")
+    VideoResponse getVideos(
+            @RequestParam(value = "game_id", required = false) String gameId,
+            @RequestParam(value = "first", required = false, defaultValue = "0") int first);
+    */
 
     @GetMapping("/clips")
     ClipResponse getClips(@RequestParam("game_id") String gameId, @RequestParam("first") int first);
