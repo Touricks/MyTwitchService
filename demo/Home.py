b93bbbad3df0968db5f9b9e56b3759215cddc521
Touricks <f36meng@gmail.com>
2024-12-09 01:34:58 -0500

    new feature +1

--- demo/Home.py
+++ demo/Home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from utils.image_utils import adjust_contrast
 
 # Set page config
 st.set_page_config(
@@ -13,8 +12,3 @@ st.title("Personalized Video Recommendation System Proposal")
 # Demo Video
 st.subheader("Please Watch the Demo video")
 st.video("path_to_demo_video.mp4")
-
-# Process and display background
-background_image_path = "background.JPG"
-contrast_factor = 0.3
-adjusted_image = adjust_contrast(background_image_path, contrast_factor)
