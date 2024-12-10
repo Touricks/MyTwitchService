104101a98fa2713668167fc5d45d0d31104f9218
Touricks <f36meng@gmail.com>
2024-12-10 02:12:31 -0500

    feature+1

--- demo/pages/0_Project_Overview.py
+++ demo/pages/0_Project_Overview.py
@@ -28,7 +28,7 @@ def set_background(image_file):
             left: 0;
             width: 100%;
             height: 100%;
-            background: rgba(255, 255, 255, 0.3);
+            background: rgba(255, 255, 255, 0.6);
             pointer-events: none;
         }}
     
